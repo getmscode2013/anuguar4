fc68f27a97554dd7e21a361edb750f2ca3456c74
getmscode2013 <getmscode2013@gmail.com>
2018-05-17 00:57:28 -0700

    4th day part 2

--- SinglePage/src/app/app.module.ts
+++ SinglePage/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule} from '@angular/forms'
+import { FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { MenubarComponent } from './menubar/menubar.component';
@@ -59,7 +59,7 @@ const routes = [
     EnquiryformComponent
   ],
   imports: [
-    BrowserModule, RouterModule.forRoot(routes), HttpClientModule, FormsModule
+    BrowserModule, RouterModule.forRoot(routes), HttpClientModule, FormsModule,ReactiveFormsModule
   ],
   providers: [CourseService, ReviewService,EnquiryService],
   bootstrap: [AppComponent]
--- SinglePage/src/app/review.service.ts
+++ SinglePage/src/app/review.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@angular/core';
 import {review } from './review';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {Observable} from 'rxjs/observable'
-import {HttpClient} from '@angular/common/http'
 
 
 @Injectable()
 export class ReviewService {
 
 
-  constructor(private http: HttpClient ) { }
+  httpOptions;
+  constructor(private http:HttpClient) { 
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    }
+  }
 
   getReviews():Observable<object>{
     return this.http.get("http://139.59.9.236:8898/rest-api/reviews")
+  }
+
+  postReviews(obj):Observable<object>{
+    return this.http.post("http://139.59.9.236:8898/rest-api/reviews",obj, this.httpOptions)
   }
 }
